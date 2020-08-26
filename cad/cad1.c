14fe533b778dbb82ce655460f01e634c150a6ee4
Lean-1 <Leanter95@outlook.com>
2020-08-26 10:54:07 +0200

    actualizacion

--- cad/cad1.c
+++ cad/cad1.c
@@ -3,13 +3,13 @@
 #include <string.h>
 
 int main (void){
-char frase[19], letra;int vez;
+char frase[19], letra;int vez, cont;
 printf("\nPor favor, escriba una frase (sólo 20 carácteres): ");
 gets(frase);fflush(stdin); //Ésta función almacena espacios. La función "scanf()" no almacena espacios.
 printf("\nPor favor, escriba una letra: ");letra=getchar();fflush(stdin);
 //Buscando la letra introducida por el usuario, en su frase introducida:
-if(strstr(frase,letra) != NULL){
-	printf("\nLa letra \'$c\' ha sido encontrada en la frase\n \"%s\"", letra, frase);
-}else { printf("\nNo ha podido encontrarse la letra introducida por el usuario...\n");}
+for(cont=0;cont=strlen(frase);cont++){if (strstr(frase, letra) != NULL){vez++;}
+else {printf("\nNo se ha podido encontrar la letra introducida en la frase.\n");}
+}
 //Fin de programa
 return 0;}
